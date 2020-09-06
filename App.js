65b6bc759bf7dfc96dd37175cc3be12f5d6377ef
unknown <usamaaagill@gmail.com>
2020-09-06 22:51:22 +0500

    update

---
+++ App.js
@@ -0,0 +1,10 @@
+
+
+import * as React from 'react';
+import {WebView} from 'react-native-webview';
+
+export default class App extends React.Component {
+  render() {
+    return <WebView source={{ uri: 'https://autism.altareqcenter.com' }} style={{ marginTop: 20 }} />;
+  }
+}  
